02bb40ee048c8f9e921176c33b44d3ce5a24bb07
AlexBurg <alex.burkh@yandex.ru>
2025-06-04 14:27:20 +0300

    fix: changed indexes to Nodes

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -3,11 +3,10 @@ import java.util.HashMap;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private static final HashMap<Integer, Integer> historyNodesIdentifiers = new HashMap<>();
+    private static final HashMap<Integer, Node> historyNodesIdentifiers = new HashMap<>();
     private Node head = null;
     private Node tail = null;
     int size = 0;
-    int nextIndex = 0;
     private class Node {
         Node prev;
         Node next;
@@ -18,22 +17,37 @@ public class InMemoryHistoryManager implements HistoryManager {
             this.next = next;
             this.data = data;
         }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getPrev() {
+            return this.prev;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getNext() {
+            return this.next;
+        }
     }
 
     @Override
     public void add(Task task) {
         remove(task.getId());
-        historyNodesIdentifiers.put(task.getId(), nextIndex++);
-        linkLast(task);
+        var newNode = new Node(null, null, task);
+        historyNodesIdentifiers.put(task.getId(), newNode);
+        linkLast(newNode);
     }
 
     @Override
     public void remove(int id) {
-        Integer index = historyNodesIdentifiers.get(id);
-        if (index != null) {
-            removeFromIndexMap(index, id);
-            removeByIndex(index);
-            nextIndex--;
+        Node node = historyNodesIdentifiers.get(id);
+        if (node != null) {
+            removeNode(node);
         }
     }
 
@@ -42,26 +56,16 @@ public class InMemoryHistoryManager implements HistoryManager {
         return getTasks();
     }
 
-    private void linkLast(Task task) {
-        if (head == null) {
-            head = new Node(null, null, task);
-            tail = head;
+    private void linkLast(Node node) {
+        if (tail == null) {
+            tail = node;
+            head = tail;
         } else {
-            Node temp = new Node(tail, null, task);
-            tail.next = temp;
-            tail = temp;
+            node.setPrev(tail);
+            tail.next = node;
+            tail = node;
         }
         size++;
-    }
-
-    private void removeByIndex(int index) {
-        var currNode = head;
-        while (index != 0) {
-            currNode = currNode.next;
-            index--;
-        }
-        removeNode(currNode);
-        size--;
     }
 
     private void removeNode(Node node) {
@@ -75,19 +79,8 @@ public class InMemoryHistoryManager implements HistoryManager {
             node.prev.next = node.next;
             node.next.prev = node.prev;
         }
+        size--;
     }
-
-    private void removeFromIndexMap(int startIndex, int id) {
-        historyNodesIdentifiers.remove(id);
-        for (var entry : historyNodesIdentifiers.entrySet()) {
-            var key = entry.getKey();
-            var value = entry.getValue();
-            if (value > startIndex) {
-                historyNodesIdentifiers.put(key, --value);
-            }
-        }
-    }
-
 
     private List<Task> getTasks() {
         var result = new ArrayList<Task>(size);
