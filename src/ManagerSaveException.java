679dad8b823e84e23f3479897df9ecefc799dfd3
AlexBurg <alex.burkh@yandex.ru>
2025-06-13 11:43:54 +0300

    fix: fixed for comments in repo

--- src/ManagerSaveException.java
+++ src/ManagerSaveException.java
@@ -1,5 +1,8 @@
 public class ManagerSaveException extends RuntimeException {
-    public ManagerSaveException() {}
+    public ManagerSaveException() {
+
+    }
+
     public ManagerSaveException(final String message) {
         super(message);
     }
