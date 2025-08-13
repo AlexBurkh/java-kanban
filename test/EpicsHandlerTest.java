f5bb31399d61e8c0abd65f4a60b6da8dfca53f88
Bast <bast@bast.ru>
2025-08-13 17:56:58 +0300

    feat: added EpicsHandler tests and prepared for subtasks

---
+++ test/EpicsHandlerTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EpicsHandlerTest extends BaseHttpHandlerTest {
+    @BeforeEach
+    public void start() throws Exception {
+        hts.start();
+        String epicBody = "{\"name\":\"epic\",\"description\":\"test epic\",\"status\":\"NEW\",\"subtasksId\":[ ]}";
+        var r1 = sendPOST(epicsUrl, epicBody);
+    }
+
+    @Test
+    public void epicStartTimeChanged() throws Exception {
+        String subtaskBody = "{\"epicId\":1,\"name\":\"subtask1\",\"description\":\"test subtask\",\"status\":\"NEW\"," +
+                "\"startTime\":\"2025-04-18T04:05:50.000\",\"duration\":40}";
+        var r1 = sendPOST(subtasksUrl, subtaskBody);
+        var r2 = sendGET(epicsUrl + "/1");
+        var epic = gson.fromJson(r2.body(), Epic.class);
+        assertEquals(201, r1.statusCode());
+        assertEquals("2025-04-18T04:05:50.000", epic.getStartTime().format(dtf));
+    }
+
+    @Test
+    public void epicEndTimeChanged() throws Exception {
+        String subtaskBody = "{\"epicId\":1,\"name\":\"subtask1\",\"description\":\"test subtask\",\"status\":\"NEW\"," +
+                "\"startTime\":\"2025-04-18T05:05:50.000\",\"duration\":40}";
+        var r1 = sendPOST(subtasksUrl, subtaskBody);
+        var r2 = sendGET(epicsUrl + "/1");
+        var epic = gson.fromJson(r2.body(), Epic.class);
+        assertEquals(201, r1.statusCode());
+        assertEquals("2025-04-18T05:45:50.000", epic.getEndTime().format(dtf));
+    }
+
+    @Test
+    public void epicStatusChanged() throws Exception {
+        String subtask1Body = "{\"epicId\":1,\"name\":\"subtask1\",\"description\":\"test subtask\",\"status\":\"NEW\"," +
+                "\"startTime\":\"2025-04-18T04:05:50.000\",\"duration\":40}";
+        String subtask2Body = "{\"epicId\":1,\"name\":\"subtask1\",\"description\":\"test subtask\",\"status\":\"NEW\"," +
+                "\"startTime\":\"2025-04-18T05:05:50.000\",\"duration\":40}";
+        var r1 = sendPOST(subtasksUrl, subtask1Body);
+        var r2 = sendPOST(subtasksUrl, subtask2Body);
+        var r3 = sendGET(epicsUrl + "/1");
+        assertEquals(201, r1.statusCode());
+        assertEquals(201, r2.statusCode());
+        Epic epics = gson.fromJson(r3.body(), Epic.class);
+        assertEquals(2, epics.getSubTasks().size());
+    }
+
+    @AfterEach
+    public void stop() {
+        hts.stop();
+    }
+}
--- test/SubtasksHandlerTest.java
+++ test/SubtasksHandlerTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-public class TasksHandlerTest extends BaseHttpHandlerTest {
+class SubtasksHandlerTest extends BaseHttpHandlerTest {
 
 
     @BeforeEach
@@ -16,8 +15,8 @@ public class TasksHandlerTest extends BaseHttpHandlerTest {
         String addTask2Body = "{\"name\":\"task1\",\"description\":\"description1\",\"status\":\"NEW\"," +
                 "\"startTime\":\"2025-08-15T14:44:28.122\",\"duration\":20}";
         try {
-            sendPOST(tasksUrl, addTask1Body);
-            sendPOST(tasksUrl, addTask2Body);
+            sendPOST(subtasksUrl, addTask1Body);
+            sendPOST(subtasksUrl, addTask2Body);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -27,8 +26,8 @@ public class TasksHandlerTest extends BaseHttpHandlerTest {
     public void addTask() throws Exception {
         String addTask2Body = "{\"name\":\"task1\",\"description\":\"description1\",\"status\":\"NEW\"," +
                 "\"startTime\":\"2025-08-15T16:44:28.122\",\"duration\":20}";
-        var addTaskResponse = sendPOST(tasksUrl, addTask2Body);
-        var getTasksResponse = sendGET(tasksUrl + "/3");
+        var addTaskResponse = sendPOST(subtasksUrl, addTask2Body);
+        var getTasksResponse = sendGET(subtasksUrl + "/3");
         var task = gson.fromJson(getTasksResponse.body(), Task.class);
         assertEquals(201, addTaskResponse.statusCode());
         assertEquals("2025-08-15T16:44:28.122", task.getStartTime().format(dtf));
@@ -44,12 +43,12 @@ public class TasksHandlerTest extends BaseHttpHandlerTest {
         String overlaps3 = "{\"id\":1,\"name\":\"task0\",\"description\":\"description0\",\"status\":\"NEW\"," +
                 "\"startTime\":\"2025-08-15T14:40:28.122\",\"duration\":20}";
 
-        var response1 = sendPOST(tasksUrl, overlaps1);
+        var response1 = sendPOST(subtasksUrl, overlaps1);
         assertEquals(406, response1.statusCode());
-        var response2 = sendPOST(tasksUrl, overlaps2);
+        var response2 = sendPOST(subtasksUrl, overlaps2);
         assertEquals(406, response2.statusCode());
-        var response3 = sendPOST(tasksUrl, overlaps3);
-        var overlaps3GetResponse = sendGET(tasksUrl + "/1");
+        var response3 = sendPOST(subtasksUrl, overlaps3);
+        var overlaps3GetResponse = sendGET(subtasksUrl + "/1");
         var task = gson.fromJson(overlaps3GetResponse.body(), Task.class);
         assertEquals(406, response3.statusCode());
         assertEquals("2025-08-15T13:44:28.122", task.getStartTime().format(dtf));
@@ -59,8 +58,8 @@ public class TasksHandlerTest extends BaseHttpHandlerTest {
     public void updateTaskTest() throws Exception {
         String updateBody = "{\"id\":1,\"name\":\"task1\",\"description\":\"edited\",\"status\":\"NEW\"," +
                 "\"startTime\":\"2025-08-15T16:44:28.122\",\"duration\":30}";
-        var updateResponse = sendPOST(tasksUrl, updateBody);
-        var getTaskResponse = sendGET(tasksUrl + "/1");
+        var updateResponse = sendPOST(subtasksUrl, updateBody);
+        var getTaskResponse = sendGET(subtasksUrl + "/1");
         var task = gson.fromJson(getTaskResponse.body(), Task.class);
         assertEquals(200, updateResponse.statusCode());
         assertEquals("2025-08-15T16:44:28.122", task.getStartTime().format(dtf));
@@ -69,9 +68,9 @@ public class TasksHandlerTest extends BaseHttpHandlerTest {
 
     @Test
     public void deleteTaskTest() throws Exception {
-        var deleteResponse = sendDelete(tasksUrl + "/1");
+        var deleteResponse = sendDelete(subtasksUrl + "/1");
         assertEquals(200, deleteResponse.statusCode());
-        var getDeletedTaskResponse = sendGET(tasksUrl + "/1");
+        var getDeletedTaskResponse = sendGET(subtasksUrl + "/1");
         assertEquals(404, getDeletedTaskResponse.statusCode());
     }
 
